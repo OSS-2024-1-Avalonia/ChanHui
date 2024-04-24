eb1ca75c10b4a40c9f1fee4c91b9cb0c705023d3
tonyhan18 <ourclub7279@gmail.com>
2024-04-24 10:51:27 +0900

    240424_Add Dialog Content

---
+++ DontWorryAvalonia/Views/MusicStoreView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace DontWorryAvalonia.Views
+{
+    public partial class MusicStoreView : UserControl
+    {
+        public MusicStoreView()
+        {
+            InitializeComponent();
+        }
+    }
+}
