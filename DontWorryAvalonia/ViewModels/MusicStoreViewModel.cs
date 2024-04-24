e1ac03774cbb2634041ed1c44dfd9cca73ae0014
tonyhan18 <ourclub7279@gmail.com>
2024-04-24 13:15:54 +0900

    240424_updated_Mock Search

--- DontWorryAvalonia/ViewModels/MusicStoreViewModel.cs
+++ DontWorryAvalonia/ViewModels/MusicStoreViewModel.cs
@@ -1,6 +1,9 @@
-﻿using ReactiveUI;
+﻿using Avalonia.Controls.Documents;
+using Avalonia.Markup.Xaml.MarkupExtensions;
+using ReactiveUI;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +11,49 @@ using System.Windows.Input;
 
 namespace DontWorryAvalonia.ViewModels
 {
+    /// <summary>
+    /// ViewModelBase Added
+    /// This adds the important extension method RaiseAndSetIfChanged to your view model, 
+    /// and will allow you to give the properties there the ability to notify changes to the view.
+    /// </summary>
     public class MusicStoreViewModel : ViewModelBase
     {
+        private string? _searchText;
+        private bool _isBusy;
+
+        public string? SearchText
+        {
+            get => _searchText;
+            // in order to implement the notification.
+            set => this.RaiseAndSetIfChanged(ref _searchText, value);
+        }
+
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set => this.RaiseAndSetIfChanged(ref _isBusy, value);
+        }
+
+        // a collection of album view models to represent the albums that the search might find,
+        // and a property to hold an album if the user selects one.
+
+        private AlbumViewModel? _selectedAlbum;
+
+        // this is a collection is capable of notification, and it is provided by the .NET framework.
+        public ObservableCollection<AlbumViewModel> SearchResults { get; } = new();
+
+        public AlbumViewModel? SelectedAlbum
+        {
+            get => _selectedAlbum;
+            set => this.RaiseAndSetIfChanged(ref _selectedAlbum, value);
+        }
+
+        // you will add some mock data directly to the view model.
+        public MusicStoreViewModel()
+        {
+            SearchResults.Add(new AlbumViewModel());
+            SearchResults.Add(new AlbumViewModel());
+            SearchResults.Add(new AlbumViewModel());
+        }
     }
 }
